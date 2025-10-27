758f89461a12e10b2521825eab67b5ba16a69afa
jsbl10 <bedoyaloaizajuansebastian@gmail.com>
2025-10-26 22:18:00 -0500

    Fase sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://duringel.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // opcional, previene errores en sitios grandes
+    changefreq: 'weekly', // le dice a Google que revise semanalmente
+    priority: 0.7, // valor por defecto de prioridad de páginas
+};
